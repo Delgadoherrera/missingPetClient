669d462b8dbf8bc3c39103678630fbb45c6713c7
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-15 00:59:33 -0300

    no json

--- src/views/MensajesArea.js
+++ src/views/MensajesArea.js
@@ -2,8 +2,7 @@ import { useEffect, useState } from "react";
 import io from "socket.io-client";
 import { MensajesService } from '../services/MensajesService'
 
-
-const socket = io('http://localhost:3002')
+const socket = io('http://localhost:4000')
 
 export default function App({ idReceptor, updateComponent, nombreEmisario }) {
   const [messages, setMessages] = useState([]);
