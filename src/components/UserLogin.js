373963f5f7339aceca711f9dc2d1560811a536ea
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-15 21:48:15 -0300

    styles map

--- src/components/UserLogin.js
+++ src/components/UserLogin.js
@@ -62,63 +62,63 @@ export default function ReactFinalFormDemo() {
     }
 
 
-};
 
 
-useEffect(function () {
-    /*    login(formData) */
-}, [onSubmit]);
 
-const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
-const getFormErrorMessage = (meta) => {
-    return isFormFieldValid(meta) && <small className="p-error">{meta.error}</small>;
-};
+    useEffect(function () {
+        /*    login(formData) */
+    }, [onSubmit]);
 
-const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text" autoFocus /></div>;
+    const isFormFieldValid = (meta) => !!(meta.touched && meta.error);
+    const getFormErrorMessage = (meta) => {
+        return isFormFieldValid(meta) && <small className="p-error">{meta.error}</small>;
+    };
+
+    const dialogFooter = <div className="flex justify-content-center"><Button label="OK" className="p-button-text" autoFocus /></div>;
 
 
-return (
-    <div className="form-demo">
-        <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
-            <div className="flex align-items-center flex-column pt-6 px-3">
-                <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
-                <h5>Bienvenido/a!</h5>
-                <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
-                    Calm Atencion Domiciliaria SRL {/* <b>{formData.name}</b> */} <br></br>Ahora puedes realizar todas tus gestiones desde nuetra web!{/*  <b>{formData.email}</b> */} {/*   */}
-                </p>
-            </div>
-        </Dialog>
-        <div className="flex justify-content-center loginForm">
-            <div className="formLogin">
-                <h5 className="text-center">Iniciar sesion</h5>
-                <Form onSubmit={onSubmit} initialValues={{ email: '', password: '' }} validate={validate} render={({ handleSubmit }) => (
-                    <form onSubmit={handleSubmit} className="p-fluid">
+    return (
+        <div className="form-demo">
+            <Dialog visible={showMessage} onHide={() => setShowMessage(false)} position="top" footer={dialogFooter} showHeader={false} breakpoints={{ '960px': '80vw' }} style={{ width: '30vw' }}>
+                <div className="flex align-items-center flex-column pt-6 px-3">
+                    <i className="pi pi-check-circle" style={{ fontSize: '5rem', color: 'var(--green-500)' }}></i>
+                    <h5>Bienvenido/a!</h5>
+                    <p style={{ lineHeight: 1.5, textIndent: '1rem' }}>
+                        Calm Atencion Domiciliaria SRL {/* <b>{formData.name}</b> */} <br></br>Ahora puedes realizar todas tus gestiones desde nuetra web!{/*  <b>{formData.email}</b> */} {/*   */}
+                    </p>
+                </div>
+            </Dialog>
+            <div className="flex justify-content-center loginForm">
+                <div className="formLogin">
+                    <h5 className="text-center">Iniciar sesion</h5>
+                    <Form onSubmit={onSubmit} initialValues={{ email: '', password: '' }} validate={validate} render={({ handleSubmit }) => (
+                        <form onSubmit={handleSubmit} className="p-fluid">
 
-                        <Field name="email" render={({ input, meta }) => (
-                            <div className="field">
-                                <span className="p-float-label p-input-icon-right">
-                                    <i className="pi pi-envelope" />
-                                    <InputText onChange={(e) => setFormData(e.target.value)} id="email" {...input} className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
-                                    <label htmlFor="email" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Correo electrónico</label>
-                                </span>
-                                {getFormErrorMessage(meta)}
-                            </div>
-                        )} />
-                        <Field name="password" render={({ input, meta }) => (
-                            <div className="field">
-                                <span className="p-float-label">
-                                    <Password autoComplete="on" onChange={(e) => setFormData(e.target.value)} id="password" {...input} toggleMask className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
-                                    <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Contraseña</label>
-                                </span>
-                                {getFormErrorMessage(meta)}
-                            </div>
-                        )} />
-                        <Button type="submit" label="Ingresar" className="mt-2 loginButton" />
-                    </form>
-                )} />
+                            <Field name="email" render={({ input, meta }) => (
+                                <div className="field">
+                                    <span className="p-float-label p-input-icon-right">
+                                        <i className="pi pi-envelope" />
+                                        <InputText onChange={(e) => setFormData(e.target.value)} id="email" {...input} className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
+                                        <label htmlFor="email" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Correo electrónico</label>
+                                    </span>
+                                    {getFormErrorMessage(meta)}
+                                </div>
+                            )} />
+                            <Field name="password" render={({ input, meta }) => (
+                                <div className="field">
+                                    <span className="p-float-label">
+                                        <Password autoComplete="on" onChange={(e) => setFormData(e.target.value)} id="password" {...input} toggleMask className={classNames({ 'p-invalid': isFormFieldValid(meta) })} />
+                                        <label htmlFor="password" className={classNames({ 'p-error': isFormFieldValid(meta) })}>Contraseña</label>
+                                    </span>
+                                    {getFormErrorMessage(meta)}
+                                </div>
+                            )} />
+                            <Button type="submit" label="Ingresar" className="mt-2 loginButton" />
+                        </form>
+                    )} />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 }
 
