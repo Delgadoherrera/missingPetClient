3baf4e1dbbef8a9a5770e7a3f1c6a48ba8baaef3
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-18 18:49:40 -0300

    asd

--- src/components/PetFound.js
+++ src/components/PetFound.js
@@ -183,10 +183,6 @@ export default function ReactFinalFormDemo() {
         {
           headers: {
             "Access-Control-Allow-Origin": "*",
-            origin: "x-requested-with",
-            "Access-Control-Allow-Headers":
-              "POST, GET, PUT, DELETE, OPTIONS, HEAD, Authorization, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Access-Control-Allow-Origin",
-            "Content-Type": "application/json",
           },
         }
       )
