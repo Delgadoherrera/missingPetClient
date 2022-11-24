632058d71720ca78b49d441954eed6b0e8bd6722
delgadoHerrera <delgado.herrera.luciano@gmail.com>
2022-11-24 14:41:28 -0300

    authKey\

--- src/services/MascotasService.js
+++ src/services/MascotasService.js
@@ -2,7 +2,14 @@
 export class MascotasService {
 
     getMascotasByIdHumano(id) {
-        return fetch(`https://backend.missingpets.art/mascotas/getById/${id}`).then(res => res.json()).then(d => d.data);
+        return fetch(`https://backend.missingpets.art/mascotas/getById/${id}`, {
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                "X-Auth-Token": localStorage.authKey,
+                "Content-Type": "application/json"
+              }
+        }).then(res => res.json()).then(d => d.data);
     }
 
     getAllMissingPets() {
